2cee56b21535b90409d94af7fc5910e5a986300f
Alanniainfotechz <alanniainfotechz@gmail.com>
2022-01-07 10:50:08 +0530

    paid module 15

---
+++ al_website_hide_price/__manifest__.py
@@ -0,0 +1,23 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Price hide for Public User',
+    'version': '15.0.1.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'author': 'Alanniainfotechz',
+    'website': '',
+    'price': 7.18,
+    'currency': 'USD',
+    'summary': 'This module helps to hide the website price for Public User',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/inherit_web_template.xml',
+
+    ],
+    'images':['static/description/cover_screenshot.png'],
+    'installable': True,
+    'auto_install': False,
+}
